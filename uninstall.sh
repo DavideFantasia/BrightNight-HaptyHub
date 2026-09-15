3121c1617604ccba8a8223be7bd56575c32a30d3
DavideFantasia <davide.fantasia04@gmail.com>
2026-09-15 17:05:28 +0200

    updated uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -5,7 +5,7 @@ echo " Disinstallazione di HaptyGraph "
 echo "==========================================="
 
 # 1. Rimozione dell'Ambiente Virtuale
-echo "[1/3] Rimozione dell'ambiente virtuale (venv)..."
+echo "[1/2] Rimozione dell'ambiente virtuale (venv)..."
 if [ -d "venv" ]; then
     rm -rf venv
     echo "  -> Cartella venv rimossa."
@@ -13,35 +13,13 @@ else
     echo "  -> Cartella venv non trovata, salto."
 fi
 
-echo "[1.5/3] Rimozione modulo ELK"
-if [ -d "node_modules" ]; then
-	npm unistall elkjs
-	rm -r -f node_modules
-	rm *.json
-	echo " -> Modulo Rimosso"
+# 2. Rimozione dell'Eseguibile
+echo "[2/2] Rimozione dell'eseguibile..."
+if [ -f "run.sh" ]; then
+    rm run.sh
+    echo "  -> Eseguibile rimosso."
 else
-	echo " -> Cartella non Rimossa"
-fi
-
-# 2. Rimozione delle Regole udev
-echo "[2/3] Rimozione delle regole hardware (richiede password amministratore)..."
-if [ -f "/etc/udev/rules.d/99-nanovna-sensor.rules" ]; then
-    sudo rm /etc/udev/rules.d/99-nanovna-sensor.rules
-    sudo udevadm control --reload-rules
-    sudo udevadm trigger
-    echo "  -> Regole udev rimosse dal sistema."
-else
-    echo "  -> Regole udev non trovate, salto."
-fi
-
-# 3. Rimozione della Scorciatoia (Icona)
-echo "[3/3] Rimozione dell'icona dal menu applicazioni..."
-DESKTOP_FILE="$HOME/.local/share/applications/HaptyGraph.desktop"
-if [ -f "$DESKTOP_FILE" ]; then
-    rm "$DESKTOP_FILE"
-    echo "  -> Icona rimossa con successo."
-else
-    echo "  -> Icona non trovata, salto."
+    echo "  -> Eseguibile non trovato, salto."
 fi
 
 echo "==========================================="
